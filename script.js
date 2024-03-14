99fcf733da5e79ecb4149d2e15f591c52063c44d
Márcio Diniz <marcio.sccp10@gmail.com>
2024-03-14 11:58:47 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// [CRUD] JavaScript BÁSICO
+const miniX = {
+
+    usuarios: [
+        {
+            username: 'marciodiniz7x',
+        }
+    ],
+    posts: [
+        {
+            owner: 'marciodiniz7x',
+            content: 'Meu primeiro post'
+        }
+    ],
+}
